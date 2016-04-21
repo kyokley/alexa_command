c9397d3899ff45128b97af9befc20f93ebdffe15
Kevin Yokley <kyokley2@gmail.com>
2016-04-21 18:28:29 -0500

    Fixing typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 
 def main():
-    # Forcing the machine to shutdown? What's the worst than can happen?
+    # Forcing the machine to shutdown? What's the worst that can happen?
     os.system('shutdown -h now')
