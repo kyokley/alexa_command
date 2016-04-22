f0568f5792d2d5ff8cd30f06b879401d64bce8af
Kevin Yokley <kyokley2@gmail.com>
2016-04-22 09:00:34 -0500

    Adding celery

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import os
 import gevent
+import time
 from ouimeaux.environment import Environment
 from ouimeaux.utils import matcher
+from celery import Celery
 
 SWITCH_NAME = 'mercury'
 matches = matcher(SWITCH_NAME)
+
+app = Celery('mercury_tasks', broker='amqp://guest@localhost//')
 
 def main():
     # Forcing the machine to shutdown? What's the worst that can happen?
     os.system('shutdown -h now')
 
-def start_up():
+def get_switch():
     env = Environment()
-    env.start()
+
+    try:
+        env.start()
+    except:
+        pass
+
     env.discover(5)
     found = None
     for switch in env.list_switches():
@@ -22,5 +31,15 @@ def start_up():
     else:
         raise Exception('Switch not found!')
 
-    found.off()
-    gevent.spawn_later(30, found.on)
+    return found
+
+def start_up():
+    switch = get_switch()
+    switch.off()
+    _switch_on.delay()
+
+@app.task
+def _switch_on():
+    switch = get_switch()
+    time.sleep(3)
+    switch.on()
