d3778b5eaf2349824d6159c69a034080d12e8d80
Kevin Yokley <kyokley2@gmail.com>
2016-10-06 15:40:38 -0500

    Returning server status on GET

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from shutdown import shutdown
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/mercury', methods=['POST'])
+@app.route('/mercury', methods=['GET', 'POST'])
 def mercury():
-    try:
-        shutdown()
-        return 'Success', 200
-    except:
-        return 'FAIL', 400
+    if request.method == 'POST':
+        try:
+            shutdown()
+            return 'Success', 200
+        except:
+            return 'FAIL', 400
+    elif request.method == 'GET':
+        return 'Shutdown server is listening', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
