138b58107243c3d1f0342359dd07da168da4290f
Kevin Yokley <kyokley2@gmail.com>
2019-11-24 09:23:25 -0600

    Remove backend from celery

--- start_up.py
+++ start_up.py
@@ -7,7 +7,7 @@ from settings import CELERY_VHOST
 SWITCH_NAME = 'Real mercury outlet'
 matches = matcher(SWITCH_NAME)
 
-mercury_task = Celery('start_up', backend='redis://localhost', broker='amqp://guest@localhost/%s' % CELERY_VHOST)
+mercury_task = Celery('start_up', broker='amqp://guest@localhost/%s' % CELERY_VHOST)
 
 def get_switch():
     env = Environment()
@@ -30,16 +30,16 @@ def get_switch():
 
 @mercury_task.task
 def _switch_on():
-    print "Getting switch"
+    print("Getting switch")
     switch = get_switch()
     time.sleep(30)
-    print "Activating switch"
+    print("Activating switch")
     switch.on()
 
 def start_up():
     switch = get_switch()
     switch.off()
-    print "Making async call"
+    print("Making async call")
     _switch_on.delay()
 
 if __name__ == '__main__':
